7a204d03a40d8613552ea307ee7a15efadb7443f
Connor Box <Connor.Box@warwick.ac.uk>
2020-02-21 10:28:01 +0000

    add

--- mem_use.py
+++ mem_use.py
@@ -8,8 +8,8 @@ from ase.units import _hbar, J, s, fs
 from scipy.integrate import cumtrapz
 
 
-file_range = np.arange(1,501).tolist()
-raw_data,bins,dimension = mel.Parse_memory_kernels('./calcs',file_range,read=True) #parse all memory data, choose range of datapoints parse 
+file_range = np.arange(1,21).tolist()
+raw_data,bins,dimension = mel.Parse_memory_kernels('./calcs',file_range)#,read=True) #parse all memory data, choose range of datapoints parse 
 
 #assumes in directories ./dir/n where n = 1,2,3 etc
 
@@ -17,7 +17,7 @@ con = connect('database.db') #connection to database that stores atoms objects a
 
 cutoffs = np.linspace(0.2,2.4,5) #all cutoff energies in eV investigating
 
-friction_indices = [16,17,18,19] #indices of friction atoms
+friction_indices = [64,65] #indices of friction atoms
 
 time_step = 2 #fs nuclear time step
 
@@ -29,6 +29,8 @@ nm_work = pp.calculate_work()
 
 nm_forces = pp.calculate_friction_force()
 
+fig_path = './figures/'
+
 fig, ax = plt.subplots(dimension,dimension,sharex='all', sharey='all')
 for i in range(dimension):
     for j in range(i,dimension):
@@ -38,7 +40,7 @@ for i in range(dimension):
 ax[0,0].legend()
 fig.set_figheight(20)
 fig.set_figwidth(20)
-fig.savefig('freq_compare.pdf')
+fig.savefig(fig_path+'freq_compare.pdf')
 
 ts=0
 fig, ax = plt.subplots(dimension,dimension,sharex='all', sharey='all')
@@ -49,7 +51,7 @@ for i in range(dimension):
 ax[0,0].legend()
 fig.set_figheight(20)
 fig.set_figwidth(20)
-fig.savefig('eta_bar.pdf')
+fig.savefig(fig_path+'eta_bar.pdf')
 
 
 fig, ax = plt.subplots(dimension,dimension,sharex='all', sharey='all')
@@ -60,7 +62,7 @@ for i in range(dimension):
 ax[0,0].legend()
 fig.set_figheight(20)
 fig.set_figwidth(20)
-fig.savefig('eta_bar_inter.pdf')
+fig.savefig(fig_path+'eta_bar_inter.pdf')
 
 
 fig, ax = plt.subplots(dimension,dimension,sharex='all', sharey='all')
@@ -71,7 +73,7 @@ for i in range(dimension):
 ax[0,0].legend()
 fig.set_figheight(20)
 fig.set_figwidth(20)
-fig.savefig('eta_inter.pdf')
+fig.savefig(fig_path+'eta_inter.pdf')
 
 fig, ax = plt.subplots(dimension,dimension,sharex='all', sharey='all')
 for i in range(dimension):
@@ -81,34 +83,47 @@ for i in range(dimension):
 ax[0,0].legend()
 fig.set_figheight(20)
 fig.set_figwidth(20)
-fig.savefig('eta_t.pdf')
+fig.savefig(fig_path+'eta_t.pdf')
+
+fig, ax = plt.subplots(dimension,dimension,sharex='all', sharey='all')
+for co in range(len(cutoffs)):
+    for i in range(dimension):
+        for j in range(i,dimension):
+            #for ts in range(pp.steps):
+            ax[i,j].plot(pp.times_up_list[co]/fs,(pp.eta_t_list[co])[ts,i,j,:],label='CO: ' + str(cutoffs[co])+'TS: '+ str(ts))
+            ax[i,j].set_xlim(0,5)
+ax[0,0].legend()
+fig.set_figheight(20)
+fig.set_figwidth(20)
+fig.text(0.5, 0.01, "Time / fs", ha='center',fontsize=15)
+fig.savefig(fig_path+'eta_t_close.pdf')
 
 fig, ax = plt.subplots(1,1)
 for atom in range(len(friction_indices)):       
     for cart in range(3):
         ax.plot(pp.inter_time_scale/fs,pp.velocities_inter[:,atom,cart],label=str(atom)+str(cart))
 ax.legend()
-fig.savefig('velocities_inter.pdf')
+fig.savefig(fig_path+'velocities_inter.pdf')
 
 fig, ax = plt.subplots(1,1)
 for atom in range(len(friction_indices)):       
     for cart in range(3):
         ax.plot(pp.inter_time_scale/fs,nm_forces[-1,:,atom,cart],label=str(atom)+str(cart))
 ax.legend()
-fig.savefig('nm_forces.pdf')
+fig.savefig(fig_path+'nm_forces.pdf')
 
 
 fig, ax = plt.subplots(1,1)
 for co in range(len(cutoffs)):
     ax.plot(pp.inter_time_scale/fs,nm_work[co,:],label=str(cutoffs[co]))
 ax.legend()
-fig.savefig('nm_work.pdf')
+fig.savefig(fig_path+'nm_work.pdf')
 
 fig, ax = plt.subplots(1,1)
 for co in range(len(cutoffs)):
     ax.plot(pp.inter_time_scale/fs,cumtrapz(nm_work[co,:],pp.inter_time_scale/fs,initial=0),label=str(cutoffs[co]))
 ax.legend()
-fig.savefig('nm_cum_work.pdf')
+fig.savefig(fig_path+'nm_cum_work.pdf')
 
 #print(pp.inter_time_scale)
 #print(nm_work)
