2bfc99b8c443a162e81d14a9be4506be26b88684
clbox <connor.box@warwick.ac.uk>
2020-02-21 20:55:12 +0000

    sfafas

--- mem_energy_loss.py
+++ mem_energy_loss.py
@@ -232,9 +232,9 @@ class Postprocessed_memory:
 
                 fit = interp1d(old_time_scale,eta_t[:,e,:],kind='linear',axis=0)
                 arr = fit(inter_time_scale)
-                print(np.shape(arr))
                 arr *= velocities_inter[:,None,j_atom,j_cart] #multiply column wise
                 integrand = (np.array([np.sum(np.diag(np.fliplr(arr), d)) for d in range(len(arr) - 1, -len(arr), -1)]))[:len(inter_time_scale)]*dt
+                print(integrand)
                 force_vec[co,:,i_atom,i_cart] += integrand
                 if i != j:
                     force_vec[co,:,j_atom,j_cart] += integrand  
