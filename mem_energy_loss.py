3b1df3c7b42907a7656d773757dd280cd4c93fba
clbox <connor.box@warwick.ac.uk>
2020-02-21 20:17:16 +0000

    fix

--- mem_energy_loss.py
+++ mem_energy_loss.py
@@ -234,8 +234,7 @@ class Postprocessed_memory:
                 arr = fit(inter_time_scale)
                 arr *= velocities_inter[:,None,j_atom,j_cart] #multiply column wise
                 integrand = [np.sum(np.diag(np.fliplr(arr), d)) for d in range(len(arr) - 1, -len(arr), -1)]
-                integrand *= dt
-                force_vec[co,:,i_atom,i_cart] += integrand
+                force_vec[co,:,i_atom,i_cart] += integrand*dt
                 if i != j:
                     force_vec[co,:,j_atom,j_cart] += integrand  
             for i in range(dimension):
