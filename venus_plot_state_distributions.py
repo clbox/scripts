639b0d33960cf0e381d4bcff71a0852b0128a889
clbox <connor.box@warwick.ac.uk>
2020-04-27 17:19:13 +0100

    c

---
+++ venus_plot_state_distributions.py
@@ -0,0 +1,69 @@
+import numpy as np
+import sys
+import os
+import glob
+from pathlib import Path
+import matplotlib
+matplotlib.use('PDF')
+import matplotlib.pyplot as plt
+from matplotlib.ticker import (MultipleLocator, FormatStrFormatter,
+                               AutoMinorLocator, MaxNLocator)
+
+filenames = sys.argv[1:]
+
+x3_exp = np.arange(0,7,1)
+v3_exp = [0.0,0.2195652173913044,0.42391304347826086,0.35434782608695653,0.0,0,0]
+
+x11_exp = np.arange(2,12)
+v11_exp = [0.047,0.099,0.18,0.18,0.18,0.12,0.068,0.052,0.036,0.025]
+
+x15_exp = np.arange(5,16)
+v15_exp = [0.115,0.1339,0.194,0.192,0.125,0.082,0.04,0.05,0.019,0.015,0.036]
+
+x16_exp = np.arange(0,17,1)
+v16_exp = [0.0,0.0,0.04,0.08,0.13,0.15,0.19,0.11,0.12,0.07,0.04,0.02,0.03,0.02,0.01,0.02,0.02]
+
+tdpt_args = {'marker' : '.','linestyle' : '-','color' : 'purple', 'label' : r'TDPT'}
+
+
+
+
+fig, ax = plt.subplots(1, 1, sharex='all',sharey='all')
+for i,filename in enumerate(filenames):
+    if 'v03' in os.path.abspath(filename):
+        ax.bar(x3_exp,v3_exp,color='black',label=r'$\nu_i=3$ exp')
+
+    if 'v11' in os.path.abspath(filename):
+        ax.bar(x11_exp,v11_exp,color='black',label=r'$\nu_i=11$ exp')
+
+    if 'v15' in os.path.abspath(filename):
+        ax.bar(x15_exp,v15_exp,color='black',label=r'$\nu_i=15$ exp')
+
+    if 'v16' in os.path.abspath(filename):
+        ax.bar(x16_exp,v16_exp,color='black',label=r'$\nu_i=16$ exp')
+
+    dis = np.loadtxt(filename)
+
+    if 'tdpt' in os.path.abspath(filename):
+        mode_args = tdpt_args
+    
+
+
+    a = ax.plot(dis[:,0],dis[:,1],**mode_args)
+
+
+
+    #if 'tdpt' in os.path.abspath(filename):
+    #    '.-',color='purple',label=r'TDPT')# $\times 2$')
+    #marker='^',linestyle='-',color='red',label=r'BOMD')
+    # marker='s',linestyle='-',color='blue',label=r'LDFA $\times 4$'))
+
+ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+ax.legend()
+
+
+fig.set_figheight(4)
+fig.set_figwidth(5)
+fig.text(0.5, 0.00, r"Final vibrational state ($\nu_f$)", ha='center',fontsize=15)
+fig.text(0.01, 0.5, 'Population', va='center', rotation='vertical',fontsize=15)
+fig.savefig('state2state.pdf',transparent=True,bbox_inches='tight')
