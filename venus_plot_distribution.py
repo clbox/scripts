2a0adda661c7aa6f9463a9df6d513c964c0e4511
clbox <connor.box@warwick.ac.uk>
2020-04-27 14:34:43 +0100

    c

--- venus_plot_distribution.py
+++ venus_plot_distribution.py
@@ -18,6 +18,9 @@ v3_exp = [0.0,0.2195652173913044,0.42391304347826086,0.35434782608695653,0.0,0,0
 x15_exp = np.arange(5,16)
 v15_exp = [0.115,0.1339,0.194,0.192,0.125,0.082,0.04,0.05,0.019,0.015,0.036]
 
+x11_exp = np.arange(2,12)
+v11_exp = [0.047,0.099,0.18,0.18,0.18,0.12,0.068,0.052,0.036,0.025]
+
 
 #mode 0 = bomd, mode 1 = ldfa, mode 2 = tdpt
 mode = int(sys.argv[1])
@@ -170,7 +173,7 @@ fig2.savefig('energy_distribution.pdf',transparent=True,bbox_inches='tight')
 ntraj_list = np.array(ntraj_list)
 fig, ax = plt.subplots(1, 1, sharex='all',sharey='all')
 ax.plot(state_list,ntraj_list/np.sum(ntraj_list),
-    '.-',color='purple',label=r'TDPT $\times 3$')
+    '.-',color='purple',label=r'TDPT')# $\times 3$')
     #marker='^',linestyle='-',color='red',label=r'BOMD')
     # marker='s',linestyle='-',color='blue',label=r'LDFA $\times 4$')
 
@@ -178,9 +181,9 @@ ax.plot(state_list,ntraj_list/np.sum(ntraj_list),
 np.savetxt('states.txt', np.c_[state_list,ntraj_list/np.sum(ntraj_list)],fmt='%1.3f')
 
 #V16
-ax.bar(x16_exp,v16_exp,color='black',label=r'$\nu_i=16$ exp')
-ax.set_xlim(0,20)
-ax.set_ylim(0,0.5)
+#ax.bar(x16_exp,v16_exp,color='black',label=r'$\nu_i=16$ exp')
+#ax.set_ylim(0,0.5)
+#ax.set_xlim(0,20)
 
 #V3
 # ax.bar(x3_exp,v3_exp,color='black',label=r'$\nu_i=3$ exp')
@@ -192,6 +195,10 @@ ax.set_ylim(0,0.5)
 # ax.set_xlim(0,17)
 # ax.set_ylim(0,0.8)
 
+#V11
+ax.bar(x11_exp,v11_exp,color='black',label=r'$\nu_i=11$ exp')
+ax.set_xlim(0,20)
+ax.set_ylim(0,0.5)
 
 ax.xaxis.set_major_locator(MaxNLocator(integer=True))
 ax.legend()
