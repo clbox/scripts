e8d29cbecf067351452abc0378443d7ed122480d
clbox <connor.box@warwick.ac.uk>
2020-04-27 13:14:21 +0100

    c

--- be_script.py
+++ be_script.py
@@ -8,15 +8,16 @@ system = read('geometry.in')
 
 default_positions  = system.get_positions()
 
+surface_height = np.max(system.get_positions()[2:,2])
 
 
 for i,height in enumerate(np.linspace(0.1,7.1,15)):
     print(height)
     new_positions = default_positions
-    new_positions[0,2] = height
-    new_positions[1,2] = height
+    new_positions[0,2] = height + surface_height
+    new_positions[1,2] = height + surface_height
     system.set_positions(new_positions)
 
-    write_aims('geometry_'+str(i)+'.in',system,friction_atoms=[0,1])
+    write_aims('geometry_{:02d}.in'.format(i),system,friction_atoms=[0,1])
 
 
