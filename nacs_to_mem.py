9812b1645ae395b908d0c747d742d4aac79e056d
Connor Box <Connor.Box@warwick.ac.uk>
2020-05-06 18:53:01 +0100

    adgaf

--- nacs_to_mem.py
+++ nacs_to_mem.py
@@ -34,7 +34,6 @@ def read_spectral_data(path):
             continue
 
     filenames.sort()
-    print(filenames)
         
     with open(path+filenames[0], "r") as f:
         for line in f:
@@ -66,8 +65,9 @@ def read_spectral_data(path):
                     file_data.append(line)
 
         a = np.loadtxt(file_data)
+
         if file_counter == 0:
-            data_sum = np.zeros([len(a[:,0])/n_spin,3])
+            data_sum = np.zeros([len(a[:,0])//n_spin,3])
         data_split = np.vsplit(a,elements*n_spin)
         c = 0
         i = 0
@@ -90,11 +90,11 @@ def read_spectral_data(path):
 
 paths = sys.argv[1:]
 for path in paths:
-    try:
-        data_split,dimension,max_e = read_spectral_data(path)
-    except:
-        print('Couldnt  read data - continuing')
-        continue
+    #try:
+    data_split,dimension,max_e = read_spectral_data(path)
+    #except:
+    #    print('Couldnt  read data - continuing')
+    #    continue
     print('max energy = {} eV'.format(max_e))
     c = -1
     i_cart = -1
