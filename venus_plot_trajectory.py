022f7ee463e96beab3aaf9f128277ac78f59a836
clbox <connor.box@warwick.ac.uk>
2020-05-10 12:39:03 +0100

    c

--- venus_plot_trajectory.py
+++ venus_plot_trajectory.py
@@ -126,5 +126,20 @@ fig.text(0.01, 0.5, r'$z$ / $\AA{}$', va='center', rotation='vertical',fontsize=
 fig.savefig('impact_'+mode+'.pdf',transparent=True,bbox_inches='tight')
 
 
-np.savetxt('impact_'+mode+'.txt',impact_geo)
+with open('impact_'+mode+'.txt', 'w') as outfile:
+    # I'm writing a header here just for the sake of readability
+    # Any line starting with "#" will be ignored by numpy.loadtxt
+    outfile.write('# Array shape: {0}\n'.format(impact_geo.shape))
+
+    # Iterating through a ndimensional array produces slices along
+    # the last axis. This is equivalent to data[i,:,:] in this case
+    for data_slice in impact_geo:
+
+        # The formatting string indicates that I'm writing out
+        # the values in left-justified columns 7 characters in width
+        # with 2 decimal places.  
+        np.savetxt(outfile, data_slice, fmt='%-7.2f')
+
+        # Writing out a break to indicate different slices...
+        outfile.write('# New traj\n')
 #ax.plot(impact_geo)
