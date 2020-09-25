53415934180e6ccc9d93b72dd1c92a1d93633b47
clbox <connor.box@warwick.ac.uk>
2020-09-25 22:02:42 +0100

    c

--- convert_to_schnet.py
+++ convert_to_schnet.py
@@ -18,14 +18,13 @@ old_database = connect(sys.argv[1])
 #available_properties = ["raw_coupling_energies","raw_coupling_elements","smear_frequency_energies","smeared_frequency_friction","markov_friction_tensor",]
 available_properties = ["friction_tensor"]
 dataset = spk.data.AtomsData("schnet.db", available_properties=available_properties)
-raw_dataset = connect(old_database)
 
 for idx in range(1,len(old_database)+1):
     print(idx)
     property_list = {}
-    mol = raw_dataset.get_atoms(id=idx)
+    mol = old_database.get_atoms(id=idx)
     #Markov
-    row = raw_dataset.get(id=idx)
+    row = old_database.get(id=idx)
     try:
         markov_tensor = string2array(row.get('ft1'))
         property_list.update({"friction_tensor": markov_tensor})
