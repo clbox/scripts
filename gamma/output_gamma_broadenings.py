ffd06740db0dae8aaa4165cb1928238924d33858
clbox <connor.box@warwick.ac.uk>
2021-11-12 12:55:04 +0000

    gamma funcs

--- gamma/output_gamma_broadenings.py
+++ gamma/output_gamma_broadenings.py
@@ -14,17 +14,18 @@ a = fc.friction_gamma_parser(aims_file=aims1,gamma_files=gamma_files1)
 sigmas = np.linspace(0.0001,1,50)
 
 modes = ['default','double_delta','prb_print','no_norm']
-
+print('begin calc')
 for mode in modes:
+    print(mode)
     tensors = []
     for sigma in sigmas:
-        b = fc.friction_tensor(a,0.0,sigma,nspin=1)
+        b = fc.friction_tensor(a,0.,sigma,nspin=1)
         tensor = b.calc_tensor(mode=mode)
         tensors.append(tensor)
     
         dimension = np.shape(tensor)[0]
     
-    
+    print('Begin output')
     tensors = np.array(tensors)
     for i in range(dimension):
         for j in range(dimension):
